f3a7950e5c7f3f6de28303998c85e637f8f96058
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2019-11-24 15:35:29 -0600

    Update Version

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ import { updateUI } from './ui.js';
 
 
 export const url = 'https://github.com/HLXII/Evolve-Autoscript';
-export const version = '1.3.3';
-export const workingVersion = '0.7.0';
+export const version = '1.3.4';
+export const workingVersion = '0.7.1';
 
 (async function() {
     console.log("Waiting for game to load...");
